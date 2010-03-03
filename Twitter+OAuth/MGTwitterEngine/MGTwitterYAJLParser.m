6fc8599668517d3daa95b85ac7d07f25ba2d74b1
Michael James <umjames29@gmail.com>
2010-03-03 02:56:48 -0500

    Made MGTwitterEngine compile with latest stable yajl (commit 9c15d72). Added iPhone static library target just to test compilation against iPhone SDK.

--- Twitter+OAuth/MGTwitterEngine/MGTwitterYAJLParser.m
+++ Twitter+OAuth/MGTwitterEngine/MGTwitterYAJLParser.m
@@ -246,7 +246,7 @@ static yajl_callbacks callbacks = {
 				0, // allowComments: if nonzero, javascript style comments will be allowed in the input (both /* */ and //)
 				0  // checkUTF8: if nonzero, invalid UTF8 strings will cause a parse error
 			};
-			_handle = yajl_alloc(&callbacks, &cfg, self);
+			_handle = yajl_alloc(&callbacks, &cfg, NULL, self);
 			if (! _handle)
 			{
 				return nil;
@@ -258,7 +258,7 @@ static yajl_callbacks callbacks = {
 				unsigned char *errorMessage = yajl_get_error(_handle, 0, [json bytes], [json length]);
 				NSLog(@"MGTwitterYAJLParser: error = %s", errorMessage);
 				[self _parsingErrorOccurred:[NSError errorWithDomain:@"YAJL" code:status userInfo:[NSDictionary dictionaryWithObject:[NSString stringWithUTF8String:(char *)errorMessage] forKey:@"errorMessage"]]];
-				yajl_free_error(errorMessage);
+				yajl_free_error(_handle, errorMessage);
 			}
 
 			// free the yajl parser
